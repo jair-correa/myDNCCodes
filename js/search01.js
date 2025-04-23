0c2fc6dde6e527c0ecf050e5f004cc9823ef7864
jair-correa <jair_correa@outlook.com.br>
2025-04-22 23:09:28 -0300

    UP codes from DNC course

---
+++ js/search01.js
@@ -0,0 +1,10 @@
+function somaValores(listaValores) {
+    let total = 0
+
+    for (let valor of listaValores) {
+        total += valor;
+    };
+    return total
+}
+console.log(somaValores([1, 2, 3, 4, 5]));
+console.log(somaValores([5, 10, 15, 20, 25]));
---
+++ js/search02.js
@@ -0,0 +1,5 @@
+function verificaSePar(valor) {
+    return valor === 0 || valor % 2 == 0;
+}
+
+console.log(verificaSePar(1));
