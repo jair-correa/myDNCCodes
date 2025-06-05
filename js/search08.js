475c1a2fce0549403b7ee03f4163de98b3d650f3
frontEnd-Jair <jair_correa@outlook.com.br>
2025-06-04 21:19:31 -0300

    Table of division with FOR

--- js/search08.js
+++ js/search08.js
@@ -9,6 +9,19 @@ for(nbr = 1; nbr <=10; nbr++){
         
     }
 }
+
+console.log("-------------------##-------------------");
+
+for(nbr = 10; nbr >= 1; nbr--){
+    console.log(`Table de division de ${nbr}`);
+    
+    for(let div = 10; div >= 0; div--){
+        console.log(`${nbr} / ${div} = ${(nbr / div).toFixed(3)}`);
+        
+    }
+}
+
+
 //for(let i = 0; i <= 10 ; i++){
 //    let search = document.getElementById("search" + i);
 //    if (search) {
