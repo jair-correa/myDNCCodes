d47c3493d6d1ebd48bc2539f4727423ed1956039
frontEnd-Jair <jair_correa@outlook.com.br>
2025-06-07 12:36:28 -0300

    push, pop, unshift and shift

---
+++ js/search16.js
@@ -0,0 +1,62 @@
+const armario = [
+    'camisetas',
+    'calcas',
+    'meias',
+    'blusas',
+    'chapeu'
+];
+
+const mesa = [
+    'livros',
+    'canetas',
+    'cadernos',
+    'laptop',
+    'colher',
+    'garfo',
+    'prato'
+    
+];
+
+console.log('\n---------push-> adiciona ao final da lista---------');
+// [inicio, fim].push(alguma) => [inicio, fim, alguma]
+armario.push('sapatos'); 
+armario.push('meias');
+armario.push('jaqueta');
+
+
+console.log("armario\t\t\t|mesa\n-------------------------");
+
+const maxLength = Math.max(armario.length, mesa.length);
+
+for (let i = 0; i < maxLength; i++) {
+    const itemArmario = armario[i] ?? '';
+    const itemMesa = mesa[i] ?? '';
+    console.log(`${itemArmario.padEnd(15)}\t|${itemMesa}`);
+}
+
+console.log('\n---------pop-> remove ultimo item da lista---------');
+// [inicio, meio, fim].pop() => [inicio, meio]
+armario.pop(); 
+mesa.pop();
+
+
+console.log("armario\t\t\t|mesa\n-------------------------");
+
+for (let i = 0; i < maxLength; i++) {
+    const itemArmario = armario[i] ?? '';
+    const itemMesa = mesa[i] ?? '';
+    console.log(`${itemArmario.padEnd(15)}\t|${itemMesa}`);
+}
+
+console.log('\n---------shift-> remove item ao comeco da lista---------');
+armario.shift(); 
+mesa.shift();
+
+
+console.log("armario\t\t\t|mesa\n-------------------------");
+
+for (let i = 0; i < maxLength; i++) {
+    const itemArmario = armario[i] ?? '';
+    const itemMesa = mesa[i] ?? '';
+    console.log(`${itemArmario.padEnd(15)}\t|${itemMesa}`);
+}
