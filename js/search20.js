1cbad9607be809aa5845b9e1cbc0b97fc3837a07
frontEnd-Jair <jair_correa@outlook.com.br>
2025-08-05 12:08:24 -0300

    feature: function some and every

--- js/search20.js
+++ js/search20.js
@@ -1,3 +1,20 @@
+const gaveta =[
+    {name: 'camisa', price: 50, material: 'elastana'},
+    {name: 'calça', price: 80, material: 'algodão'},
+    {name: 'sapato', price: 120, material: 'couro'},
+    {name: 'meia', price: 10, material: 'algodão'},
+    {name: 'cinto', price: 30, material: 'couro'},
+];
+
+const hasElastana = gaveta.some((item) => item.material === 'elastana')
+console.log(hasElastana);
+
+const allClothesAreBelow100 = gaveta.every((item) => item.price < 130)
+console.log(allClothesAreBelow100, gaveta);
+
+const hasCouro = gaveta.every((item)=> item.material.every ==='couro') //verifica se todos os itens tem couro
+console.log(hasCouro);
+/*
 //some 
 let lista = [1,2,3,4,5,6,7,'8'];
 
@@ -14,3 +31,4 @@ const isNumber = lista.every((item) => {
     return typeof item === 'number';
 })
 console.log(isNumber);
+*/
