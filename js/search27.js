68b492749e07a3070d44ae599526dfd0e72e85b2
frontEnd-Jair <jair_correa@outlook.com.br>
2025-08-21 19:57:32 -0300

    chore: update code with async and await

--- js/search27.js
+++ js/search27.js
@@ -1,3 +1,28 @@
+let promessaDeCompra = async () => new Promise((resolve, reject)=>{
+    console.log('Estou indo comprar o caderno pra voce...');
+
+    setTimeout(()=>{
+       let sucesso = true;
+       if(sucesso) resolve('Comprei o caderno');
+        else reject('A loja esta fechada')
+        
+        console.log('Fazendo outras coisas...');
+    
+    },5000); //5s de tempo de espera
+})
+
+
+async function main(){
+    try{
+        const compra = await promessaDeCompra()
+        console.log(compra)
+    }catch(err){
+        console.log('Não pude concluir a compra. ',err);
+        
+    }
+}
+main()
+/*
 console.log('Eu preciso de caderno novo');
 
 let promessaDeCompra = new Promise((resolve, reject)=>{
@@ -17,3 +42,5 @@ console.log('Fazendo outras coisas...');
 promessaDeCompra
 .then((mensagem)=> console.log(mensagem))
 .catch((err)=>console.log(err))
+
+*/
