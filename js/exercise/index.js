ff66e9a202532621541e97befde977f25e13fd20
frontEnd-Jair <jair_correa@outlook.com.br>
2025-08-21 22:09:03 -0300

    chore: Atualizando o codigo com  checkbox input

--- js/exercise/index.js
+++ js/exercise/index.js
@@ -3,7 +3,25 @@ let tasks = [
     {id:2, description:'passear como cachorro', checked:false},
     {id:3, description:'fazer almoco', checked:false},
 ]
+const getCheckboxInput = ({id, description, checked}) => {
+    const checkbox = document.createElement('input');
+    const label = document.createElement('label');
+    const wrapper = document.createElement('div');
+    const checkboxId = `${id}-checkbox`;
 
+    checkbox.type = 'checkbox';
+    checkbox.id = `${id}-checkbox`;
+    checkbox.checked = task.checked;
+
+    label.textContent = description;
+    label.htmlFor = checkboxId;
+
+    wrapper.className= 'checkbox-label-container';
+    wrapper.appendChild(checkbox);
+    wrapper.appendChild(label);
+
+    return wrapper;
+}
 window.onload = function(){
     tasks.forEach((task) =>{//para cada tarefa
         const list = document.getElementById('all-list');
