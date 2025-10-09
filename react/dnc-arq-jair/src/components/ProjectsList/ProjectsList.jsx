0a4fa08afc642600b9c0fa54d2a2c0271ecf814c
frontEnd-Jair <jair_correa@outlook.com.br>
2025-10-09 19:37:54 -0300

    feat(projectsList): add hooks for updating status and content

--- react/dnc-arq-jair/src/components/ProjectsList/ProjectsList.jsx
+++ react/dnc-arq-jair/src/components/ProjectsList/ProjectsList.jsx
@@ -1,9 +1,31 @@
+//importacoes de hooks para atualizar o estado e atualizacao do conteudo e buscar dados na API
+import { useState, useEffect } from 'react';
+
 //ASSETS
 import './ProjectsList.css';
 import LikedFilled from '@assets/like-filled.svg';
 import Liked from '@assets/like.svg';
 
+//UTIlS
+import { getApiData } from '../../services/js/apiServices';
+
 function ProjectsList() {
+  //estado para armazenar os dados da API
+  //eslint-disable-next-line no-unused-vars
+  const [projects, setProjects] = useState();
+
+  //useEffect para buscar os dados da API quando o componente for montado
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const projectsResponse = await getApiData('projects');
+        setProjects(projectsResponse);
+      } catch {
+        setProjects([]);
+      }
+    };
+    fetchData();
+  }, []); //garantir que a funcao seja executada apenas uma vez quando o componente dor montado
   return (
     <div className='projects-section'>
       <div className='projects-hero'>
