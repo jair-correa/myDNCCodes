fa865548e91f923cfe7d00ea50db9e3a742a8d5c
frontEnd-Jair <jair_correa@outlook.com.br>
2025-10-29 21:02:40 -0300

    feat: add welcome function with patient status check

---
+++ introducao-typescript/src/funcoes.ts
@@ -0,0 +1,15 @@
+function desejarBoasVindas(nome: string, idade: string, internado: boolean): void {
+  console.log(`Boas vindas ${nome}`);
+  console.log(`Voce tem ${nome} anos de idade`);
+
+  if (internado == true) {
+    console.log('Voce esta internado');
+  } else {
+    console.log('Voce nao esta internado');
+  }
+  console.log('---');
+}
+
+desejarBoasVindas('Jair', '34', false);
+desejarBoasVindas('Maria', '55', true);
+desejarBoasVindas('Luciano', '23', false);
